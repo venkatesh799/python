7143ec9a25ab2fd138da787fe80041fcb8a1e446
venkatesh thirunagiri <35319751+venkatesh799@users.noreply.github.com>
2019-07-09 10:39:46 +0530

    Create find vs rfind vs rindex.py

---
+++ find vs rfind vs rindex.py
@@ -0,0 +1,5 @@
+s="python is best for AI and python is best for ML"
+target="python"
+print(s.find(target))  # it will give the first occurence of substring i.e 0 index else returns -1
+print(s.rindex(target))   # it will give the last occurece of substring i.e 26th index else Throws Error
+print(s.rfind(target))    # it will give the last occurece of substring i.e 26th index else returns -1
