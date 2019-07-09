dca5ea24bdbb91b94854ff8aec402e67ca6a47c9
venkatesh thirunagiri <35319751+venkatesh799@users.noreply.github.com>
2019-07-09 11:20:01 +0530

    Update Seperating characters and specials.py

--- Seperating characters and specials.py
+++ Seperating characters and specials.py
@@ -2,7 +2,7 @@ s="Gee#ks$%For$Geeks@"
 ch=[]
 sp=[]
 for x in range(len(s)):
-    if s[x]>='a' and s[x]<='z' or s[x]>='A' and s[x]<='Z':
+    if s[x]>='a' and s[x]<='z' or s[x]>='A' and s[x]<='Z':  # we can use str.isalpha() also
         ch.append(s[x])
     else:
         sp.append(s[x])
