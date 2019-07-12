ef6086949133f9a4c864fa154b56feb071af9ffe
venkatesh thirunagiri <35319751+venkatesh799@users.noreply.github.com>
2019-07-12 10:50:44 +0530

    Create str to list and list to str.py

---
+++ str to list and list to str.py
@@ -0,0 +1,5 @@
+s="hello world i'm venkatesh"
+n=s.split(' ')   # string to list
+print(n)           # ['hello', 'world', "i'm", 'venkatesh']
+l=' '.join(n)     # list to string
+print(l)             #hello world i'm venkatesh
